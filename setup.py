a3ea535e0cb8be8f4ad56211f7c4acfb9fc432e2
Friedrich42 <39532283+Friedrich42@users.noreply.github.com>
2020-06-07 15:31:34 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,4 +23,4 @@ PACKAGE_INFO = {}
 with open(VERSION_FILENAME) as f:
     exec(f.read(), PACKAGE_INFO)
 
-setuptools.setup(name="opentelemetry.ext.exporter-cloud-trace", version=PACKAGE_INFO["__version__"])
+setuptools.setup(version=PACKAGE_INFO["__version__"])
